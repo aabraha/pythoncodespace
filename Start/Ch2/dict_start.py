e3e5743996d669b419b40513754aee26350b5fe8
Assefa Abraha <55986320+aabraha@users.noreply.github.com>
2025-11-15 22:36:09 +0000

    Code changes to chapter 2 exercise

--- Start/Ch2/dict_start.py
+++ Start/Ch2/dict_start.py
@@ -3,21 +3,35 @@
 
 
 # Dictionary: a key-value data structure
+myDict = {
+  "one":1,
+  "two": 2,
+  3: "three",
+  4.5: ["four","point","five"]
+}
 
+# print(myDict)
 
 # dictionaries are accessed via keys
-
+# print(myDict["one"])
+# print(myDict[3])
 
 # you can also set dictionary data by creating a new key
 
-
+myDict["seven"] = 7
+# print(myDict)
 # Trying to access a nonexistent key will produce an error
-
+# print(myDict["blabla"])
 
 # To avoid this, you can use the "in" operator to see if a key exists
-
+# print("two" in myDict)
+# print("blabla" in myDict)
 
 # You can retrieve all of the keys and values from a dictionary
-
+# print(myDict)
+# print(myDict.keys())
+# print(myDict.values())
 
 # You can also iterate over all the items in a dictionary
+for key, val in myDict.items():
+  print(key,val)
--- Start/Ch2/sequence_start.py
+++ Start/Ch2/sequence_start.py
@@ -1,28 +1,47 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for complex types
 
-# Sequences: Lists and Tuples
+# Sequences: Lists , Tuples and Sets
 # These are -- surprise -- sequences of values
-
-
+myList = [0, 1, "two", 3.5, False]
+# print(len(myList))
 # to access a member of a sequence type, use []
-
+# print(myList[2])
+# print(myList[-1])
+# myList[0] = 10
+# print(myList)
+#print(myList[:-1])
 
 # add a list to another list
-
+# anotherList = [6, 7, 8]
+# print(myList + anotherList)
+# # a string is an immutable char list
+# myStr = "This is a string"
+# print(myStr[2])
 
 # use slices to get parts of a sequence
-
+# print(myList)
+# print("Sliced [1,4] ->", myList[1:4])
+# print("Sliced [1,4,2] ->", myList[1:4:2]) # start = index 1, end = index 3, step amount=2
+# print("Sliced default [::] ->", myList[::])
+# print("Sliced [::2] ->", myList[::2]) # default start: 0, default end: last index, step=2
 
 # you can use slices to reverse a sequence
-
+# print("Reverse a list [::-1] ->", myList[::-1])
 
 # Tuples are like lists, but they are immutable
-
+myTuple = (0, 1, 2, "three")
+# print(myTuple)
+# print(myTuple[1])
+# myTuple[1] = "one" # you can't change the values
 
 # Sets are also sequences, but they contain unique values
-
+mySet = {1, 2, 3, 2, "hey"}
+# print(mySet) # only holds unique values
 # Set, however, can not be indexed like lists or tuples
-# print(myset[0]) # this will cause an error
+# print(mySet[0]) # this will cause an error
 
 # Test for membership
+print(1 in myList)
+print(3 in myTuple)
+print(5 in mySet)
