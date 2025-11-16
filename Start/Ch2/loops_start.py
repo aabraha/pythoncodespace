0db744805f101aaeb2960d8ca35eaf9362c47ea3
Assefa Abraha <55986320+aabraha@users.noreply.github.com>
2025-11-16 18:47:03 +0000

    Code changes to add conditional and loop

--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -5,15 +5,27 @@
 x = 0
 
 # define a while loop
-
+# while x < 5:
+#   print(x)
+#   x+=1
 
 # define a for loop
-
+days = ["mon","tue","wed","thu","fri","sat","sun"]
 
 # use a for loop over a collection
-
+# for d in days:
+#   print(d)
 
 # use the break and continue statements
-
+# for d in days:
+#   if d =="wed":
+#     break
+#   print(d)
+for d in days:
+  if d =="wed":
+    continue
+  print(d)
 
 # using the enumerate() function to get an index and an item
+for i, d in enumerate(days):
+  print(i, d)
