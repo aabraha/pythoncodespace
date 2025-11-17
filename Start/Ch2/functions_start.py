0fa39afff359028886841647934172c783172425
Assefa Abraha <55986320+aabraha@users.noreply.github.com>
2025-11-17 00:58:05 +0000

    Code changes to work with function

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,53 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+def helloFunc():
+  print("hello world!")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
+#helloFunc() #using the func defined
 
 # function that takes parameters
+def helloFunc(greetings):
+  print("hello world!")
+  name = input("What is your name? ")
+  print(greetings, name)
 
+# helloFunc("How are you doing")
+# helloFunc("What's up")
 
 # function that returns a value
+def cube(x):
+  return x*x*x
+result = cube(3)
+# print("the cube value is",result)
 
+# function with default value for a parameter
+def helloFunc(greetings, name=None):
+  print("hello world!")
+  if name == None:
+    name = input("What is your name? ")
+  print(greetings, name)
 
-# function with default value for an parameter
+# helloFunc("Nice to meet you", "Assefa") 
+# helloFunc("Nice to meet you") 
+# helloFunc(name = "Abraha", greetings="Nice to meet you") 
 
 
 # function with variable number of parameters
+def multiAdd(*args):
+  result = 0
+  for x in args:
+    result += x
+  return result
+
+# print(multiAdd(4,5,10,4,10))
+
+# the variable param should be at the end of params
+def multiAddParams(start, *args):
+  result = 0
+  for x in args:
+    result += x
+  return result * start
+
+print(multiAddParams(10, 4,5,10,4,10))
