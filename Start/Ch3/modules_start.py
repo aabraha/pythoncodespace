86c6c165365f0c459f0811efa360809146f8b9fe
Assefa Abraha <55986320+aabraha@users.noreply.github.com>
2025-11-20 20:26:44 +0000

    Code changes working on modules

--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -1,28 +1,33 @@
-# LinkedIn Learning Python course by Joe Marini
+#
 # Working with modules of code
 
 # import the math module, which contains features for working with mathematics
-
+import math
+print("The sqaure root of 81 is", math.sqrt(81))
+print("The cube root of 27 is", math.cbrt(27))
 
 # import a specific part of the module so you can refer to it more easily
-
+from math import pi
+print("PI is equal to", pi)
 
 # import a module and give it a different name
-
+import random as r
+print("Random num =", r.randint(100,200))
 
 # the math module contains lots of pre-built functions
 
 
 # in addition to functions, some modules contain useful constants 
-
+print("The mathematical constant e =", math.e)
 
 # Generate a random number between 100 and 200
-
+print("Random num =", r.randint(100,200))
 
 # try some of the math functions for yourself here:
+print("The 3 power of 2 is", math.pow(3, 2))
 
 # Use the 3rd party tabulate module to print tabulated data:
-
+from tabulate import tabulate
 # Sample data
 data = [
   ["Product", "Price", "Stock"],
@@ -32,3 +37,13 @@ data = [
 ]
 
 # Create a formatted table
+print("\n pipe table format\n")
+print(tabulate(data,headers="firstrow", tablefmt="pipe"))
+print("\n github table format\n")
+print(tabulate(data,headers="firstrow", tablefmt="github"))
+print("\n outline table format\n")
+print(tabulate(data,headers="firstrow", tablefmt="outline"))
+print("\n grid table format\n")
+print(tabulate(data,headers="firstrow", tablefmt="grid"))
+print("\n simple grid table format\n")
+print(tabulate(data,headers="firstrow", tablefmt="simple_grid"))
