8c8698e8ae7b2b3e3266a90721e4c6501c447ebc
Assefa Abraha <55986320+aabraha@users.noreply.github.com>
2025-12-08 23:48:05 +0000

    Code changes working with date info

--- Start/Ch5/dates_start.py
+++ Start/Ch5/dates_start.py
@@ -1,24 +1,28 @@
 #
 # Example file for working with date information
-# LinkedIn Learning Python course by Joe Marini
-#
 
-
-from datetime import date
-from datetime import datetime
+from datetime import date #imports date class from datetime module
+from datetime import datetime #imports datetime class from datetime module
 
 ## DATE OBJECTS
 # Get today's date from the simple today() method from the date class
-
+today = date.today()
+print("Today's date is", today)
 
 # print out the date's individual components
-
+print("Date components", today.day, today.month, today.year)
 
 # retrieve today's weekday (0=Monday, 6=Sunday)
-
+print("Today's weekday #:", today.weekday())
+days = ["mon","tue","wed","thu","fri","sat","sun"]
+print("Which is ", days[today.weekday()])
 
 ## DATETIME OBJECTS
 # Get today's date from the datetime class
-
+today = datetime.now()
+print("Today's date and time is", today)
 
 # Get the current time
+#time = datetime.time(datetime.now())
+time = datetime.time(today)
+print("The current time is", time)
